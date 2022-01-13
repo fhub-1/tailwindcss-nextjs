cb844e3af2e6aa38b8d8ed1c171dcf6ed29a56ce
fhub-1 <flutterhub1.21.0@gmail.com>
2022-01-13 13:06:58 +0300

    first commit

---
+++ .history/components/Header_20220113123410.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+            
+        </nav>
+    )
+}
---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <Link>
+            <a>
+                
+            </a>
+            </Link>
+        </div>
+    </nav>
+  );
+}
